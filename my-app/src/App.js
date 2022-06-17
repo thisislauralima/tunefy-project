7b18912ba5675b01959140c317d67640f0725910
Laura Lima <lauraolima2000@gmail.com>
2022-06-17 15:54:29 -0300

    feat: :construction: set routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,12 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={ pages.Login } />
+      <Route exact path="/search" component={ pages.Search } />
+      <Route exact path="/album/:id" component={ pages.Album } />
+      <Route exact path="/favorites" component={ pages.Favorites } />
+      <Route exact path="/profile" component={ pages.Profile } />
+      <Route exact path="/profile/edit" component={ pages.ProfileEdit } />
+      <Route exact path="*" component={ pages.NotFound } />
     </Switch>
   );
 }
