95f585dfc3d7a2c03edbe76c628a3a14d3aaab64
Laura Lima <lauraolima2000@gmail.com>
2022-06-17 15:59:14 -0300

    feat: :sparkles: login page

--- my-app/src/pages/Login/Login.js
+++ my-app/src/pages/Login/Login.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { EMAIL_REGEX, EMAIL_MIN_LENGTH } from '../../utils/constants';
 
 export default function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [isDisabled, setButton] = useState(true);
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const isEmailCorrect = EMAIL_REGEX.test(email);
+    const checkEmailLength = email.length > EMAIL_MIN_LENGTH;
+    const checkPassword = password.length >= EMAIL_MIN_LENGTH;
+    if (isEmailCorrect && checkEmailLength && checkPassword) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  }, [setButton, email, password]);
+
+  const switchToSearch = () => {
+    history.push('/search');
+  }
+
   return (
-    <div>Login Page</div>
+    <>
+    <h1>Login</h1>
+      <div>
+        <input
+          placeholder='email'
+          type="text"
+          onChange={({ target: { value }}) => setEmail(value)}
+        />
+        <input
+          placeholder='senha'
+          type="password"
+          onChange={({ target: { value }}) => setPassword(value)}
+        />
+        <button
+          disabled={ isDisabled }
+          onClick={ switchToSearch }
+          type="button">Entrar</button>
+      </div>
+    </>
   );
 }
