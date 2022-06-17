c17f1a1a1736ff4abe049b0836a10dd75251ada9
Laura Lima <lauraolima2000@gmail.com>
2022-06-17 15:57:58 -0300

    feat: :art: create and set components

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,7 +1,19 @@
+import Album from './Album/Album';
+import Favorites from './Favorites/Favorites';
 import Login from './Login/Login';
+import NotFound from './NotFound/NotFound';
+import Profile from './Profile/Profile';
+import ProfileEdit from './ProfileEdit/ProfileEdit';
+import Search from './Search/Search';
 
 const pages = {
+  Album,
+  Favorites,
   Login,
+  NotFound,
+  Profile,
+  ProfileEdit,
+  Search,
 }
 
 export default pages;
---
+++ my-app/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const EMAIL_REGEX = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+
+export const EMAIL_MIN_LENGTH = 6;
