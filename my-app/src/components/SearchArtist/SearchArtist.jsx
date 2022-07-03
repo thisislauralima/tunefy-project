3368b056cbb24acddcc385983fe46e5f441b633b
Laura Lima <lauraolima2000@gmail.com>
2022-07-02 22:57:20 -0300

    feat: :zap: add logic to SearchArtist component

--- my-app/src/components/SearchArtist/SearchArtist.jsx
+++ my-app/src/components/SearchArtist/SearchArtist.jsx
@@ -1,21 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
+import searchAlbumsAPI from '../../services/searchAlbum';
 import tunesContext from '../../context/tunesContext';
 
 export default function SearchArtist() {
   const {
-    searchedArtist,
-    setSearchedArtist,
+    albumsResult,
+    setAlbumsResult,
+    setSearch,
+    setDisplaySearch
   } = useContext(tunesContext);
+
+  const [searchedArtistInput, setSearchedArtistInput] = useState('');
+  
+  const searchArtist = async () => {
+    const albums = await searchAlbumsAPI(searchedArtistInput);
+    setSearch(albums);
+    setSearchedArtistInput('');
+    setAlbumsResult(true);
+    setDisplaySearch(true);
+  }
 
   return (
     <div>
-      <input type="text" onChange={ ({ target: { value }}) => {
-        setSearchedArtist(value);
-      }}/>
+      <input
+        type="text"
+        onChange={ ({ target: { value }}) => {
+          setSearchedArtistInput(value);
+        }}
+        value={ searchedArtistInput }
+      />
       <button
         type="button"
-        disabled={ searchedArtist.length < 2 ? true : false }
-      >pesquisar</button>
+        disabled={ searchedArtistInput.length < 2 ? true : false }
+        onClick={ searchArtist }
+      >
+        pesquisar
+      </button>
+      {
+        albumsResult && <span>Resultados encontrados para sua busca:</span>
+      }
     </div>
   );
 }
