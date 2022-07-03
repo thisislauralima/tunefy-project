0964d689ca2d92a096019ae2fbd0c42d56baba74
Laura Lima <lauraolima2000@gmail.com>
2022-07-02 22:54:30 -0300

    refactor: :wrench: change Album to jsx

---
+++ my-app/src/pages/Album/Album.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ClickedAlbum from '../../components/ClickedAlbum/ClickedAlbum';
+
+export default function Album() {
+  return (
+    <>
+      <div>Album Page</div>
+      <ClickedAlbum />
+    </>
+  );
+}
